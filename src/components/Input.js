e0d7b15ffbce3609e255ec93dfc56fbed4a11ebf
alireza mhm <alireza.mhm01@gmail.com>
2022-05-16 20:05:39 +0430

    input component was added

---
+++ src/components/Input.js
@@ -0,0 +1,31 @@
+import React, { useRef, useEffect } from 'react';
+import styles from '../styles/input/input.module.css';
+
+const Input = ({ changer, cref, initialValue, placeholder, type, isCorrect, incorrectCause }) => {
+    const { inputContainer, input, inputPlaceholder, activeInput, dangerInput, dangerCause } = styles;
+    const containerRef = useRef();
+    useEffect(() => {
+        if (isCorrect) {
+            containerRef.current.classList.remove(dangerInput)
+            if (initialValue) {
+                containerRef.current.classList.add(activeInput)
+            } else {
+                containerRef.current.classList.remove(activeInput)
+            }
+        } else {
+            containerRef.current.classList.add(dangerInput)
+        }
+
+    })
+    return (
+        <>
+            <div className={inputContainer} ref={containerRef}>
+                <input className={input} ref={cref} onChange={changer} value={initialValue} type={type} spellCheck="false" />
+                <span className={inputPlaceholder} onClick={() => { cref.current.focus() }}>{placeholder}</span>
+                <div className={dangerCause} style={{ visibility: `${isCorrect ? 'hidden' : 'visible'}` }}>{incorrectCause}</div>
+            </div>
+        </>
+    )
+};
+
+export default Input;

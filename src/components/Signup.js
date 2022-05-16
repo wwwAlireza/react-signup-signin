2784a05adf7d3dde0aef012fd4901721543aa33b
alireza mhm <alireza.mhm01@gmail.com>
2022-05-16 20:06:24 +0430

    Signup bases were prepared and set up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from "../styles/signup/signup.module.css";
+import Input from './Input';
 
 const Signup = () => {
+    const { container, signupContainer, title, inputsContainer } = styles;
 
-    const { container, signupContainer, title } = styles;
+    let [username, setUsername] = useState('');
+    let usernameRef = useRef();
+    const usernameHandler = () => {
+        setUsername(usernameRef.current.value);
+    }
     return (
         <>
             <div className={container}>
                 <div className={signupContainer}>
                     <h1 className={title}>Sign Up</h1>
+                    <div className={inputsContainer}>
+                        <Input changer={usernameHandler} cref={usernameRef} initialValue={username} type="text" placeholder="username" isCorrect={true} incorrectCause='' />
+                    </div>
                 </div>
             </div>
         </>

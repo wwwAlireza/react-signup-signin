0436c16c9f65558ad70b3a94152758a9c3b8a3ea
alireza mhm <alireza.mhm01@gmail.com>
2022-05-16 20:26:26 +0430

    Instead of using useState, useReducer was used

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,22 +1,36 @@
-import React, { useState, useRef } from 'react';
-import styles from "../styles/signup/signup.module.css";
+import React, { useState, useRef, useReducer } from 'react';
+import styles from '../styles/signup/signup.module.css';
 import Input from './Input';
+
+const usernameInitial = {
+    value: '',
+    isCorrect: true,
+    incorrectCause: ''
+}
+const usernameReducer = (state, action) => {
+    if (action.action == 'setValue') {
+        return { ...state, value: action.value }
+    } else if (action.action == 'setIsCorrect') {
+        return { ...state, isCorrect: action.value, incorrectCause: action.cause | '' }
+    }
+}
 
 const Signup = () => {
     const { container, signupContainer, title, inputsContainer } = styles;
 
-    let [username, setUsername] = useState('');
     let usernameRef = useRef();
     const usernameHandler = () => {
-        setUsername(usernameRef.current.value);
+        dispatchUsername({ action: 'setValue', value: usernameRef.current.value })
     }
+
+    const [username, dispatchUsername] = useReducer(usernameReducer, usernameInitial)
     return (
         <>
             <div className={container}>
                 <div className={signupContainer}>
                     <h1 className={title}>Sign Up</h1>
                     <div className={inputsContainer}>
-                        <Input changer={usernameHandler} cref={usernameRef} initialValue={username} type="text" placeholder="username" isCorrect={true} incorrectCause='' />
+                        <Input changer={usernameHandler} cref={usernameRef} initialValue={username.value} type="text" placeholder="username" isCorrect={username.isCorrect} incorrectCause={username.incorrectCause} />
                     </div>
                 </div>
             </div>

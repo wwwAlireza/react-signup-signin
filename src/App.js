ff415ebacab1f4466660ad8ab02a558177a5cb6b
alireza mhm <alireza.mhm01@gmail.com>
2022-07-01 13:37:22 +0430

    account route was added to the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/main/main.css";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+import Account from "./components/Account";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </>
   )

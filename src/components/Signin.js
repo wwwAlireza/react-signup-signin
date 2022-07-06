427d06c5ef31d15243aaf57c140844ddd4a0e290
alireza mhm <alireza.mhm01@gmail.com>
2022-07-06 12:40:10 +0430

    The signin component and its style files are built and configured and ready to be coded

---
+++ src/components/Signin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useTitle from '../hooks/useTitle';
+import styles from "../styles/signin/signin.module.css";
+
+const Signin = () => {
+    useTitle("Sign in");
+    return (
+        <>
+
+        </>
+    )
+};
+
+export default Signin;

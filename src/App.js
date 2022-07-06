e4675ae936a8248620094e36c049e88dd3654dea
alireza mhm <alireza.mhm01@gmail.com>
2022-07-06 12:36:59 +0430

    Add the Signin component to app and routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/main/main.css";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+import Signin from "./components/Signin"
 import Account from "./components/Account";
 import { Route, Routes } from "react-router-dom";
 
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/account" element={<Account />} />
       </Routes>
     </>
